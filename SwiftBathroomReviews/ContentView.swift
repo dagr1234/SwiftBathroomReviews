6391f281b7903049a5ed4195ccd63a6c8c4efd27
dagr1234 <david.grossman1234@gmail.com>
2022-12-25 22:28:54 -0500

    Initial Commit

---
+++ SwiftBathroomReviews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftBathroomReviews
+//
+//  Created by David Grossman on 12/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftBathroomReviews/SwiftBathroomReviewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBathroomReviewsApp.swift
+//  SwiftBathroomReviews
+//
+//  Created by David Grossman on 12/25/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBathroomReviewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
